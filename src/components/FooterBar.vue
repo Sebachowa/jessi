54737bcfce8b0e8a8fc0fa8f52b5847cdcefc6a7
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2023-02-14 19:02:40 +0100

    Fix profil link

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -55,10 +55,10 @@
             </div>
           </li>
           <li class="nav__item" v-if="user">
-              <router-link to="/user-profile/:id" class="nav__link">
-                  <svg class= "nav__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M19 2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h4l3 3 3-3h4a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-7 3c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zM7.177 16c.558-1.723 2.496-3 4.823-3s4.266 1.277 4.823 3H7.177z"></path></svg>
-                  <span class="nav__name">Profile</span>
-              </router-link>
+            <router-link :to="{ name: 'selectedUserView', params: { id: user._id }}" class="nav__link">
+                <svg class= "nav__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M19 2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h4l3 3 3-3h4a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-7 3c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zM7.177 16c.558-1.723 2.496-3 4.823-3s4.266 1.277 4.823 3H7.177z"></path></svg>
+                <span class="nav__name">Profile</span>
+            </router-link>
           </li>
                     
         </ul>
